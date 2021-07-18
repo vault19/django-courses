6b35944da0f24d7b0b27edabcd624be26a3596a7
Richard Kellner <richard.kellner@gmail.com>
2021-07-18 21:14:23 +0200

    Updated admin to be able to see and modify data more efficiently, minor model update and regenerated migrations

--- courses/admin.py
+++ courses/admin.py
@@ -3,63 +3,121 @@ from django.urls import reverse
 from django.utils.http import urlencode
 from django.utils.html import format_html
 
-from courses.models import Courses, Curriculums, CurriculumDetails, Runs, Artefact, PeerReview, Certificates
+from courses.models import Course, Curriculum, CurriculumDetail, Run, Artefact, Review, Certificate
 
 
-class CoursesAdmin(admin.ModelAdmin):
-    list_display = ("name", "state", "view_runs_link", "view_curriculums_link")
+class CurriculumInline(admin.TabularInline):
+    model = Curriculum
+    extra = 0
 
-    def view_runs_link(self, obj):
-        count = obj.runs_set.count()
+
+class RunInline(admin.TabularInline):
+    model = Run
+    extra = 0
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("name", "state", "view_run_link", "view_curriculum_link",)
+    list_filter = ("state",)
+    inlines = (RunInline, CurriculumInline,)
+
+    def view_run_link(self, obj):
+        count = obj.run_set.count()
         url = (
-                reverse("admin:courses_runs_changelist")
+                reverse("admin:courses_run_changelist")
                 + "?"
                 + urlencode({"courses__id": f"{obj.id}"})
         )
         return format_html('<a href="{}">{} Run(s)</a>', url, count)
 
-    view_runs_link.short_description = "Runs"
+    view_run_link.short_description = "Runs"
 
-    def view_curriculums_link(self, obj):
-        count = obj.runs_set.count()
+    def view_curriculum_link(self, obj):
+        count = obj.curriculum_set.count()
         url = (
-                reverse("admin:courses_curriculums_changelist")
+                reverse("admin:courses_curriculum_changelist")
                 + "?"
                 + urlencode({"courses__id": f"{obj.id}"})
         )
         return format_html('<a href="{}">{} Curriculum(s)</a>', url, count)
 
-    view_curriculums_link.short_description = "Curriculums"
+    view_curriculum_link.short_description = "Curriculums"
 
 
-class CurriculumsAdmin(admin.ModelAdmin):
-    pass
+class CurriculumDetailInline(admin.TabularInline):
+    model = CurriculumDetail
+    extra = 1
 
 
-class CurriculumDetailsAdmin(admin.ModelAdmin):
-    pass
+@admin.register(Curriculum)
+class CurriculumAdmin(admin.ModelAdmin):
+    list_display = ("title", "course", "length", "view_details_link")
+    inlines = (CurriculumDetailInline,)
+
+    def view_details_link(self, obj):
+        count = obj.curriculumdetail_set.count()
+
+        return format_html('{} Detail(s)', count)
+
+    view_details_link.short_description = "Details"
 
 
-class RunsAdmin(admin.ModelAdmin):
-    pass
+class ArtefactInline(admin.TabularInline):
+    model = Artefact
+    fields = ("curriculum", "author", "title", "description", "data")
+    readonly_fields = ("curriculum", "author", "title", "description", "data")
+    extra = 0
 
 
+@admin.register(Run)
+class RunAdmin(admin.ModelAdmin):
+    list_display = ("title", "course", "start", "end", "view_artefacts_link")
+    list_filter = ("start",)
+    inlines = (ArtefactInline,)
+
+    def view_artefacts_link(self, obj):
+        count = obj.artefact_set.count()
+        url = (
+                reverse("admin:courses_artefact_changelist")
+                + "?"
+                + urlencode({"run__id": f"{obj.id}"})
+        )
+        return format_html('<a href="{}">{} Artefact(s)</a>', url, count)
+
+    view_artefacts_link.short_description = "Artefacts"
+
+
+class ReviewInline(admin.TabularInline):
+    model = Review
+    fields = ("author", "title", "description")
+    readonly_fields = ("author", "title", "description")
+    extra = 0
+
+
+@admin.register(Artefact)
 class ArtefactAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "curriculum", "run", "view_reviews_link")
+    list_filter = ("run",)
+    inlines = (ReviewInline,)
+
+    def view_reviews_link(self, obj):
+        count = obj.review_set.count()
+        url = (
+                reverse("admin:courses_review_changelist")
+                + "?"
+                + urlencode({"artefact__id": f"{obj.id}"})
+        )
+        return format_html('<a href="{}">{} Review(s)</a>', url, count)
+
+    view_reviews_link.short_description = "Reviews"
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("title", "artefact", "author")
+
+
+@admin.register(Certificate)
+class CertificateAdmin(admin.ModelAdmin):
     pass
-
-
-class PeerReviewAdmin(admin.ModelAdmin):
-    pass
-
-
-class CertificatesAdmin(admin.ModelAdmin):
-    pass
-
-
-admin.site.register(Courses, CoursesAdmin)
-admin.site.register(Curriculums, CurriculumsAdmin)
-admin.site.register(CurriculumDetails, CurriculumDetailsAdmin)
-admin.site.register(Runs, RunsAdmin)
-admin.site.register(Artefact, ArtefactAdmin)
-admin.site.register(PeerReview, PeerReviewAdmin)
-admin.site.register(Certificates, CertificatesAdmin)
--- courses/models.py
+++ courses/models.py
@@ -9,7 +9,7 @@ COURSE_STATE = (
 )
 
 
-class Courses(models.Model):
+class Course(models.Model):
     name = models.CharField(max_length=250)
     description = models.TextField()
     state = models.CharField(max_length=1, choices=COURSE_STATE, default='D')
@@ -18,32 +18,33 @@ class Courses(models.Model):
         return f"{self.name}"
 
 
-class Curriculums(models.Model):
+class Curriculum(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField(help_text=_('Explain what will user learn in this lesson.'))
-    course = models.ForeignKey(Courses, on_delete=models.CASCADE)
-    length = models.IntegerField(blank=True, null=True, help_text='')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    length = models.IntegerField(blank=True, null=True, help_text=_('Number of days that curriculum will be open.'))
+    # require_artifact = models.BooleanField()
 
     def __str__(self):
-        return f"{self.title}"
+        return f"{self.course}: {self.title}"
 
 
-class CurriculumDetails(models.Model):
+class CurriculumDetail(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField(blank=True, null=True, help_text=_('Describe study material.'))
     data = models.FileField(help_text=_('Upload study material (document, video, image).'))
-    curriculum = models.ForeignKey(Curriculums, on_delete=models.CASCADE)
+    curriculum = models.ForeignKey(Curriculum, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.title}"
 
 
-class Runs(models.Model):
+class Run(models.Model):
     title = models.CharField(max_length=250)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     start = models.DateField()
     end = models.DateField(blank=True, null=True)
-    course = models.ForeignKey(Courses, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.title}"
@@ -53,15 +54,15 @@ class Artefact(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField(blank=True, null=True, help_text=_('Describe what you have learned.'))
     data = models.FileField(blank=True, null=True, help_text=_('Upload proof of your work (document, video, image).'))
-    curriculum = models.ForeignKey(Curriculums, on_delete=models.CASCADE)
-    run = models.ForeignKey(Runs, on_delete=models.CASCADE)
+    curriculum = models.ForeignKey(Curriculum, on_delete=models.CASCADE)
+    run = models.ForeignKey(Run, on_delete=models.CASCADE)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.title}"
+        return f"{self.curriculum}: {self.title}"
 
 
-class PeerReview(models.Model):
+class Review(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField(blank=True, null=True, help_text=_('Describe your opinion about the artefact.'))
     artefact = models.ForeignKey(Artefact, on_delete=models.CASCADE)
@@ -74,9 +75,9 @@ class PeerReview(models.Model):
         return f"{self.title}"
 
 
-class Certificates(models.Model):
+class Certificate(models.Model):
     data = models.FileField()
-    run = models.ForeignKey(Runs, on_delete=models.CASCADE)
+    run = models.ForeignKey(Run, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     class Meta:
