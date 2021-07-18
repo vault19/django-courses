e8ecf39298a654d1666d8645331da9fbb9df6f94
Richard Kellner <richard.kellner@gmail.com>
2021-07-18 13:26:41 +0200

    Added initial Amdin interface

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from courses.models import Courses, Curriculums, CurriculumDetails, Runs, Artefact, PeerReview, Certificates
 
-# Register your models here.
+
+class CoursesAdmin(admin.ModelAdmin):
+    pass
+
+
+class CurriculumsAdmin(admin.ModelAdmin):
+    pass
+
+
+class CurriculumDetailsAdmin(admin.ModelAdmin):
+    pass
+
+
+class RunsAdmin(admin.ModelAdmin):
+    pass
+
+
+class ArtefactAdmin(admin.ModelAdmin):
+    pass
+
+
+class PeerReviewAdmin(admin.ModelAdmin):
+    pass
+
+
+class CertificatesAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Courses, CoursesAdmin)
+admin.site.register(Curriculums, CurriculumsAdmin)
+admin.site.register(CurriculumDetails, CurriculumDetailsAdmin)
+admin.site.register(Runs, RunsAdmin)
+admin.site.register(Artefact, ArtefactAdmin)
+admin.site.register(PeerReview, PeerReviewAdmin)
+admin.site.register(Certificates, CertificatesAdmin)
