0652862fea9f5cefaf4f83cf62d99b7a6b851f09
Richard Kellner <richard.kellner@gmail.com>
2021-07-23 20:26:44 +0200

    Added initial views and templates to display data

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,19 @@
+import datetime
+from django.http import Http404
 from django.shortcuts import render
+from courses.models import Run
 
-# Create your views here.
+
+def index(request):
+    courses_list = Run.objects.filter(course__state='O').filter(start__gt=datetime.datetime.today()).order_by('-start')
+    context = {'courses_list': courses_list}
+
+    return render(request, 'courses/index.html', context)
+
+
+def detail(request, id):
+    try:
+        course = Run.objects.get(pk=id)
+    except Run.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'courses/detail.html', {'course': course})
