5db9caec975046f43f152906c9c1c4e2f14b2792
Marek Mansell <marek.mansell@gmail.com>
2021-10-22 10:54:47 +0200

    Added Review Form

--- courses/forms.py
+++ courses/forms.py
@@ -1,11 +1,17 @@
 from django.forms import ModelForm, Form, CharField, HiddenInput
-from courses.models import Submission
+from courses.models import Submission, Review
 
 
 class SubmissionForm(ModelForm):
     class Meta:
         model = Submission
         fields = ["title", "description", "data"]
+
+
+class ReviewForm(ModelForm):
+    class Meta:
+        model = Review
+        fields = ["title", "description", "accepted"]
 
 
 class SubscribeForm(Form):
