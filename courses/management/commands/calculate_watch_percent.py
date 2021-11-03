37b544f9577908d0e21f8f76ef515d0e4d08de2b
Richard Kellner <richard.kellner@gmail.com>
2021-11-03 06:22:01 +0100

    Updated Black formatting

--- courses/management/commands/calculate_watch_percent.py
+++ courses/management/commands/calculate_watch_percent.py
@@ -13,8 +13,8 @@ class Command(BaseCommand):
         for submission in Submission.objects.filter(id=31).all():
             # Rename stored data: watched_video_time_range changed to video_watched_time_range (all starts with video)
             if submission.metadata and "watched_video_time_range" in submission.metadata:
-                submission.metadata['video_watched_time_range'] = submission.metadata["watched_video_time_range"]
-                del(submission.metadata["watched_video_time_range"])
+                submission.metadata["video_watched_time_range"] = submission.metadata["watched_video_time_range"]
+                del submission.metadata["watched_video_time_range"]
 
             if submission.metadata and "video_watched_time_range" in submission.metadata:
                 if submission.lecture.metadata and "video_duration" in submission.lecture.metadata:
