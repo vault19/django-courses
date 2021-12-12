23b16fec22380076c4eea6208a2336903c05b678
Richard Kellner <richard.kellner@gmail.com>
2021-12-12 12:27:42 +0100

    Updated and improoved comand for generatoin certificates

--- courses/management/commands/generate_certificate.py
+++ courses/management/commands/generate_certificate.py
@@ -5,16 +5,20 @@ from courses.models import Run, Certificate
 
 
 class Command(NotifyCommand):
-    help = "Notify (send email) users that new chapter has opened (chapter start == today +/- time_delta)."
+    help = "Generate certificates and notify (send email) to users that are eligible to receive certificate for " \
+           "course that has already ended."
 
     def add_arguments(self, parser):
         super().add_arguments(parser)
         parser.add_argument(
-            "--user_ids", type=str, help="Comma separated list of IDs for which certificates will be generated."
+            "--user_ids", type=str, help="Comma separated list of IDs for which certificates will be generated "
+                                         "(no matter if they are eligible for the certificate)."
         )
         parser.add_argument(
             "--run_ids", type=str, help="Comma separated list of Run IDs for which certificates will be generated."
         )
+        parser.add_argument("--fake", action="store_true",
+                            help="Fake run. Does not store anything to DB, and wont send any emails.")
 
     def handle(self, *args, **options):
         if options["time_delta"]:
@@ -32,7 +36,7 @@ class Command(NotifyCommand):
         if options["user_ids"]:
             user_ids = options["user_ids"].split(",")
 
-        runs = Run.objects.filter(start__lt=datetime.today()).filter(end__gt=datetime.today())
+        runs = Run.objects.filter(end__lt=notify_date)
 
         if run_ids:
             runs = runs.filter(id__in=run_ids)
@@ -41,32 +45,62 @@ class Command(NotifyCommand):
             if options["verbosity"] >= 2:
                 self.stdout.write(f"Found {runs.count()} active run(s).")
 
+            counter = 0
+
             for run in runs.all():
-                counter = 0
+                start_count = counter
                 self.mail_subject = run.get_setting("COURSES_NOTIFY_CERTIFICATE_EMAIL_SUBJECT")
                 self.mail_body = run.get_setting("COURSES_NOTIFY_CERTIFICATE_EMAIL_BODY")
                 self.mail_body_html = run.get_setting("COURSES_NOTIFY_CERTIFICATE_EMAIL_HTML")
 
+                if options["verbosity"] >= 3:
+                    self.stdout.write(self.style.WARNING(run))
+
                 for user in run.users.all():
-                    create_cert = False
 
-                    if not user_ids:
-                        # TODO: Define automatic completion conditions!
-                        create_cert = True
-                    if str(user.id) in user_ids:
-                        create_cert = True
+                    if str(user.id) in user_ids or run.passed(user.id):
+                        user_certificates = Certificate.objects.filter(run=run).filter(user=user)
 
-                    if create_cert:
-                        self.mail_template_variables["certificate"] = Certificate.objects.get_or_create(
-                            run=run, user=user
-                        )[0]
-                        self.send_email(user, run, verbosity=1, delay=0, confirm=options["confirm"])
-                        counter += 1
+                        if user_certificates.count() == 0:
 
-                if counter == 0 and options["verbosity"] >= 1:
-                    self.stdout.write(f"{run} hasnt any chapter starting {notify_date}.")
+                            if options["verbosity"] >= 1:
+                                self.stdout.write(f"Generating certificate for: ", ending='')
 
+                                if options["verbosity"] >= 3:
+                                    self.stdout.write(self.style.SUCCESS(f"{user.first_name} {user.last_name}"),
+                                                      ending='')
+                                    self.stdout.write(f' (ID: {user.id})')
+                                else:
+                                    self.stdout.write(self.style.SUCCESS(f"{user.first_name} {user.last_name}"),
+                                                      ending='')
+                                    self.stdout.write(' in ', ending='')
+                                    self.stdout.write(self.style.WARNING(run))
+
+                            if not options["fake"]:
+                                cert = Certificate(run=run, user=user)
+                                cert.save()
+
+                                self.mail_template_variables["certificate"] = cert
+                                self.send_email(user, run, verbosity=options["verbosity"], delay=options["delay"],
+                                                confirm=options["confirm"])
+
+                            counter += 1
+
+                if options["verbosity"] >= 3:
+                    if counter - start_count == 0:
+                        self.stdout.write("No (new) certificates has been generated.")
+                    else:
+                        self.stdout.write('Generated ', ending='')
+                        self.stdout.write(self.style.SUCCESS(counter - start_count), ending='')
+                        self.stdout.write(' new certificates.')
+
+            if options["verbosity"] >= 1:
+                self.stdout.write(self.style.SUCCESS(f"Total: {counter}"), ending='')
+                self.stdout.write(" (new) certificates has been generated.")
         else:
             self.stdout.write(self.style.SUCCESS("Nothing to do..."))
+
+        if options["fake"] and options["verbosity"] >= 1:
+            self.stdout.write(self.style.ERROR("FAKE: Nothing has been stored to DB and NO email was send!"))
 
         super().handle(*args, **options)
