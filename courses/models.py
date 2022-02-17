48b37c265c1feadc05e07a9b7617bbe4eb7ac48b
Marek Mansell <marek.mansell@gmail.com>
2022-02-17 09:26:11 +0100

    Added course duration and required skills to models

--- courses/models.py
+++ courses/models.py
@@ -97,6 +97,9 @@ class Course(models.Model):
     video = models.CharField(max_length=250, null=True, blank=True)
     tag = models.CharField(max_length=250, null=True, blank=True)
     ribbon = models.CharField(max_length=250, null=True, blank=True)
+    course_length = models.CharField(max_length=250, null=True, blank=True)
+    required_skills = models.CharField(max_length=250, null=True, blank=True)
+
 
     def __str__(self):
         return f"{self.title}"
